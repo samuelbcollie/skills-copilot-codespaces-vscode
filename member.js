806bc77114f4a7916bdd79883cd868cad8e18412
Sam Collie <samuelbcollie@outlook.com>
2025-02-26 16:19:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
